79d0320f8d3b4406683f5af85704bacc2ce2eb57
0Trickster0 <37331560+0Trickster0@users.noreply.github.com>
2018-10-24 17:39:32 +0800

    250

---
+++ SoWeDate/Assets/Script/Main/ChoiceBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceBox : MonoBehaviour {
+
+    public int skipDialogNumber;
+    public int nextChoice;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    //按下按钮后执行的方法
+    public void Button()
+    {
+        DialogManager.Instance.i += skipDialogNumber;//跳过文本顺序表中一定的节点
+        DialogManager.Instance.shownText = "";
+        GameObject.Find("DialogManager").SendMessage("ShowDialog");
+        GameObject.Find("DialogManager").SendMessage("SetDialogText", DialogManager.Instance.nodeArray[DialogManager.Instance.i].text);
+        DialogManager.Instance.isDisabled = false;
+        ChoiceManager.Instance.k += nextChoice;//跳过选项顺序表中一定的节点
+        GameObject[] boxes=GameObject.FindGameObjectsWithTag("ChoiceBox");
+        foreach(var go in boxes)//消除所有选项框
+        {
+            Destroy(go);
+        }
+    }
+}
---
+++ SoWeDate/Assets/Script/Main/ChoiceManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using LitJson;
+
+
+public class ChoiceManager : MonoBehaviour {
+
+    public GameObject choiceBox;
+    public string text;
+    public List<Choice> choiceArray = new List<Choice>();
+    public string fileName;
+
+    public int k;
+    private static ChoiceManager instance;
+    
+    //每个选项的结构体
+    public struct Choice
+    {
+        public int id;
+        public int boxesNumber;
+        public int[] skipDialogNumber;//每个选项跳过对话的数量
+        public int[] nextChoice;//每个选项跳过选项的数量
+    }
+
+    //单例
+    public static ChoiceManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+
+	// Use this for initialization
+	void Awake () {
+        Instance = this;
+        choiceArray = ReadJsonFile(fileName);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //设置一个选项框
+    public void SetChoiceBox(int position, string choiceText,int skipDialogNumber,int nextChoice)
+    {
+        GameObject cB = Instantiate(choiceBox, transform.position + new Vector3(0, 160*position, 0), Quaternion.identity);
+        cB.transform.parent = GameObject.Find("ChoiceCanvas").gameObject.transform;
+        Text buttonText = cB.GetComponentInChildren<Text>();
+        buttonText.text = choiceText;
+        cB.GetComponent<ChoiceBox>().skipDialogNumber = skipDialogNumber;
+        cB.GetComponent<ChoiceBox>().nextChoice = nextChoice;
+    }
+
+    //设置多个选项框
+    public void SetMultipleBoxes(int boxesNumber,int[] skipDialogNumberArray,int[] nextChoiceArray)
+    {
+        int j = 1;
+        for (j = 1; j <= boxesNumber; j++)
+        {
+            SetChoiceBox(2 - j, DialogManager.Instance.nodeArray[DialogManager.Instance.i+j].text,skipDialogNumberArray[j-1],nextChoiceArray[j-1]);   
+        }
+    }
+
+    //设置所有选项框
+    public void SetAllChoiceBoxes()
+    {
+        //for (k = 0; k < choiceArray.Count; k++)
+        //{
+        //    SetMultipleBoxes(choiceArray[k].boxesNumber, choiceArray[k].skipDialogNumber);
+        //}
+        SetMultipleBoxes(choiceArray[k].boxesNumber, choiceArray[k].skipDialogNumber,choiceArray[k].nextChoice);
+
+    }
+
+    //读取包含每个选项的参数的Json文件
+    public List<Choice> ReadJsonFile(string fileName)
+    {
+        List<Choice> choiceList = new List<Choice>();
+        choiceList = JsonMapper.ToObject<List<Choice>>(File.ReadAllText(Application.dataPath + fileName));
+        return choiceList;
+    }
+}
